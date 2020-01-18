5cd6917084c4b8623d722f0d11717194055f0d95
rahul <rahul.30213021@gmail.com>
2020-01-18 21:59:17 +0530

    Text editing and installing dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +14,21 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      amount: 99.99,
+      date: DateTime.now(),
+      title: 'Homies things',
+    ),
+    Transaction(
+      id: 't2',
+      amount: 69.99,
+      date: DateTime.now(),
+      title: 'Lassuns',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,25 +36,69 @@ class MyHomePage extends StatelessWidget {
           title: Text('Expencess App'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Card(
               child: Container(
-                child: Text('CARD'),
+                child: Text('CHART'),
                 width: double.infinity,
                 color: Colors.blueAccent,
               ),
               elevation: 5,
             ),
-            Card(
-              child: Text('List of expencs'),
-              color: Colors.red,
+            Column(
+              //Transactions
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    //Row
+                    children: <Widget>[
+                      Container(
+                        //Amount
+                        margin: EdgeInsets.symmetric(
+                          vertical: 5,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                        ),
+                        padding: EdgeInsets.all(5),
+                        child: Text(
+                          '\$${tx.amount}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        //Title and date
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontSize: 13,
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
+                              // shadows: Shadow(),
+                            ),
+                          ),
+                          Text(tx.date.toString(),
+                              style: TextStyle(
+                                fontSize: 11,
+                                color: Colors.grey,
+                                // shadows: Shadow(),
+                              )),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
             ),
-            Card(
-              child: Text('data'),
-              color: Colors.green,
-            )
           ],
         ));
   }
